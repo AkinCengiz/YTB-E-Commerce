48a38df081946bcab4b5ffc96e406de2f221342b
akincengiz <akincengiz@outlook.com.tr>
2025-02-14 22:40:08 +0300

    UI Pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
+import Brands from './components/layout/brands/Brands'
+import CampaingSingle from './components/layout/campaing/CampaingSingle'
 import Footer from './components/layout/footer/Footer'
 import Header from './components/layout/header/Header'
 import Policy from './components/layout/policy/Policy'
+import SingleProductPage from './pages/SingleProductPage'
+// import ShopPage from './pages/ShopPage'
+// import ContactPage from './pages/ContactPage'
+// import CartPage from './pages/CartPage'
+// import AccountPage from './pages/AccountPage'
+// import SingleBlogPage from './pages/SingleBlogPage'
+//import HomePage from './pages/HomePage'
 
 function App() {
   
   return (
     <>
       <Header />
-      <h1>E-Commerce</h1>
+      {/* <HomePage /> */}
+      {/* <SingleBlogPage /> */}
+      {/* <AccountPage /> */}
+      {/* <CartPage /> */}
+      {/* <ContactPage /> */}
+      {/* <ShopPage /> */}
+      <SingleProductPage />
+      <Brands />
+      <CampaingSingle />
       <Policy />
       <Footer />
     </>
