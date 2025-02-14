bfda57f3ddad36429d800c0efca8189e298c6e3e
akincengiz <akincengiz@outlook.com.tr>
2025-02-14 21:17:23 +0300

    E-commerce initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Footer from './components/layout/footer/Footer'
+import Header from './components/layout/header/Header'
+import Policy from './components/layout/policy/Policy'
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <h1>E-Commerce</h1>
+      <Policy />
+      <Footer />
+    </>
+  )
+}
+
+export default App
