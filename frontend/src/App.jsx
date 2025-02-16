0839d91eeb01cf016f575a332dd3254270e0ad5d
akincengiz <akincengiz@outlook.com.tr>
2025-02-16 17:14:56 +0300

    Search Modals created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 import Brands from './components/layout/brands/Brands'
 // import CampaingSingle from './components/layout/campaing/CampaingSingle'
-import Footer from './components/layout/footer/Footer'
-import Header from './components/layout/header/Header'
 import Policy from './components/layout/policy/Policy'
 // import CartPage from './pages/CartPage'
 // import AccountPage from './pages/AccountPage'
 // import SingleBlogPage from './pages/SingleBlogPage'
-// import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage"
 
 // import BlogsPage from "./pages/BlogsPage"
-import SingleProductPage from './pages/SingleProductPage'
+// import SingleProductPage from './pages/SingleProductPage'
 // import ShopPage from './pages/ShopPage'
 // import ContactPage from './pages/ContactPage'
 
@@ -18,18 +16,16 @@ function App() {
   
   return (
     <>
-      <Header />
-      {/* <HomePage /> */}
+      <HomePage />
       {/* <BlogsPage /> */}
       {/* <AccountPage /> */}
       {/* <CartPage /> */}
       {/* <ContactPage /> */}
       {/* <ShopPage /> */}
-      <SingleProductPage />
+      {/* <SingleProductPage /> */}
       {/* <SingleBlogPage /> */}
       <Brands />
       <Policy />
-      <Footer />
     </>
   )
 }
---
+++ frontend/src/layouts/SiteLayout.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import Header from "../components/layout/header/Header"
+import Footer from "../components/layout/footer/Footer"
+import Search from "../components/modals/Search";
+import Dialog from "../components/modals/Dialog";
+import { useState } from "react";
+
+function SiteLayout({children}) {
+    const [isSearchShow, setIsSearchShow] = useState(false);
+  return (
+    <div className="site-layout">
+        <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+        <Dialog />
+        <Header setIsSearchShow={setIsSearchShow}/>
+            { children }
+        <Footer/>
+    </div>
+  )
+}
+
+export default SiteLayout
+
+SiteLayout.propTypes = {
+    children : PropTypes.node
+}
